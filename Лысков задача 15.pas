a91f79bdb665c6c71b30b44ba361ce64e884e545
eqsll <144461926+eqsll@users.noreply.github.com>
2023-09-25 23:18:56 +0300

    Add files via upload

---
+++ Лысков задача 15.pas
@@ -0,0 +1,19 @@
+﻿program haz;
+var
+  n, sum, pro, k: integer;
+begin
+  write('Введите число: ');
+  readln(n);
+  sum := 0;
+  pro := 1;
+  while n > 0 do 
+  begin
+    sum := sum + n mod 10;
+    pro := pro * (n mod 10);
+    n := n div 10;
+    k:= k + 1;
+  end;
+  writeln('Количество цифр = ', k);
+  writeln('Сумма цифр = ', sum);
+  writeln('Произведение цифр = ', pro);
+end.
