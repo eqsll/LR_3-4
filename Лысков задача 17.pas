1b33f3a5adc3c3a941d7786e8034d0178c2fdeba
eqsll <144461926+eqsll@users.noreply.github.com>
2023-09-25 23:00:26 +0300

    Add files via upload

---
+++ Лысков задача 17.pas
@@ -0,0 +1,10 @@
+﻿program p;
+var n,i,kolvo:integer;
+begin
+write ('Введите количество минут ');
+read (n);
+kolvo:=1;
+for i:=1 to n do 
+  kolvo:=kolvo*2;
+writeln('Бактерий станет ', kolvo);
+end.
