894bfe4787d513c3c4e4f31dcf1768a6ca0da1aa
eqsll <144461926+eqsll@users.noreply.github.com>
2023-09-25 22:11:44 +0300

    Add files via upload

---
+++ Лысковв задача 8.pas
@@ -0,0 +1,11 @@
+﻿program summa;
+var sum : real ;
+    i, n : integer ;
+begin
+write ('Введите конечное значение: ');
+read (n);
+sum:= 0;
+for i:= 1 to n do
+sum:= sum + 1/i;
+writeln ('Сумма = ', sum:3:3);
+end.
