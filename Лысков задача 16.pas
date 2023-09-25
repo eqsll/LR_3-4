be1233d2a323132c294e628ccaf6c67bd9d85dd3
eqsll <144461926+eqsll@users.noreply.github.com>
2023-09-25 22:48:41 +0300

    Add files via upload

---
+++ Лысков задача 16.pas
@@ -0,0 +1,15 @@
+﻿var n,j:int64;
+begin
+writeln ('Введите число: ');
+readln (n);
+j:=2;
+while n>1 do
+  begin
+while n mod j = 0 do
+  begin
+writeln ('Множиткль: ', j);
+n:= n div j;
+end;
+j:= j+1;
+end;
+end.
