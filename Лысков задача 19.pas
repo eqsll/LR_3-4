4e530445de870efb28126e701b06c6a1ddb83830
eqsll <144461926+eqsll@users.noreply.github.com>
2023-09-25 14:26:03 +0300

    Add files via upload

---
+++ Лысков задача 19.pas
@@ -0,0 +1,13 @@
+﻿program perever;
+var a, b, c: integer;
+begin
+  write ('Введите любое число: ');
+  read (a);
+  b:= 0;
+  while a > 0 do begin
+  c:= a mod 10;
+  b:= b*10+c;
+  a:= a div 10;
+  end;
+  write ('Перевернутое число: ', b);
+end.
