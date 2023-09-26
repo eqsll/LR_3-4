8ce6a47b46f34123b2db55b0d050bf63296280ae
eqsll <144461926+eqsll@users.noreply.github.com>
2023-09-26 09:26:49 +0300

    Add files via upload

---
+++ Лысков задача 10.pas
@@ -0,0 +1,14 @@
+﻿var i, j, k, l, m, n, s : integer ;
+begin
+s:= 0;
+for i:= 0 to 9 do
+for j:= 0 to 9 do 
+for k:= 0 to 9 do
+for l:= 0 to 9 do
+for m:= 0 to 9 do
+for n:= 0 to 9 do
+if (i + j + k) = (l + m + n) then
+if i + j + k = 13 then
+s:= s + 1;
+Writeln (s);
+end.
